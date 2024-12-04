b287e7a3d73903ac4b61b0b85367a145a8625c13
Siddhu-K <siddharth@krishya.co>
2024-12-05 00:51:16 +0530

    update routing for TestSub component

--- src/app/test/test.routes.ts
+++ src/app/test/test.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'test_sub/:sub_id',
+    path: 'test_sub',
     loadComponent: () =>
       import('./test-sub/test-sub.component').then((m) => m.TestSubComponent),
   },
